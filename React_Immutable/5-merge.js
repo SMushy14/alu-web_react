356f0dc112ef9a37433a9a3e7d0e1a9cc4488c8d
SMushy14 <s.mushy1@alustudent.com>
2026-01-31 13:19:52 +0200

    immutable

--- React_Immutable/5-merge.js
+++ React_Immutable/5-merge.js
@@ -1,13 +1,5 @@
 import { List, Map } from 'immutable';
 
-export function concatElements(page1, page2) {
-  const list1 = List(page1);
-  const list2 = List(page2);
-  return list1.concat(list2);
-}
+export const concatElements = (page1, page2) => List(page1).concat(List(page2));
 
-export function mergeElements(page1, page2) {
-  const object1 = Map(page1);
-  const object2 = Map(page2);
-  return object1.merge(object2);
-}
+export const mergeElements = (page1, page2) => Map(page1).merge(Map(page2));
